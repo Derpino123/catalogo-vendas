44e400e218fbacba1b8bf4f54bf812f5d436c083
David Santos <daavidkarlos@gmail.com>
2019-06-09 15:18:34 -0300

    Ajustando notação ClienteUpdateValidator

--- src/main/java/br/com/davsantos/entities/dto/ClienteDTO.java
+++ src/main/java/br/com/davsantos/entities/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.davsantos.entities.Cliente;
+import br.com.davsantos.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
