af789aa98f9070e83151b4d1677b0451a82df09e
David <daavidkarlos@gmail.com>
2019-06-26 15:46:12 -0300

    Implantação de autenticacao de login e senha

--- src/main/java/br/com/davsantos/config/SecurityConfig.java
+++ src/main/java/br/com/davsantos/config/SecurityConfig.java
@@ -7,13 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import br.com.davsantos.security.JWTAuthenticationFilter;
+import br.com.davsantos.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment environment;
 
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	private static final String PUBLIC_MATCHERS[] = { "/h2-console/**"};
 	
-	private static final String PUBLIC_MATCHERS_GET[] = { "/produtos/**", "/categorias/**" };
+	private static final String PUBLIC_MATCHERS_GET[] = { "/produtos/**", "/categorias/**", "/clientes/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -39,6 +51,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.anyRequest()
 		.authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	CorsConfigurationSource corsConfigurationSource() {
