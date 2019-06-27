abf67de8337e80520de27e0b0f359b3be6ed4326
David <David@DESKTOP-FD6L2F5>
2019-06-27 00:03:23 -0300

    Iniciando um refresh token

---
+++ src/main/java/br/com/davsantos/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package br.com.davsantos.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.davsantos.security.JWTUtil;
+import br.com.davsantos.security.User;
+import br.com.davsantos.services.UserS;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		User user = UserS.authenticate();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
